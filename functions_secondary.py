0c79da54ca70ae8a3fb23bea71d44fcadefb64d7
Soren <vlad1998ss@mail.ru>
2023-08-29 22:21:43 +0300

    it turns out to log in to facebook (but this is not always):
    functions_secondary.py:
    - changed chrome_options and checking selenium driver anonymity on:
    https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html
    - replaced useragent
    
    main.py:
    - added filling authorization form by selenium via saved proxies

--- functions_secondary.py
+++ functions_secondary.py
@@ -14,19 +14,42 @@ def get_parsing_indexes(start_ind, end_ind, count_bot):
 
 
 # Функция получения драйвера по url страницы
-def create_driver():
+def create_driver(proxy=False):
     # WEB_DRIVER_PATH = 'Drivers/geckodriver-v0.32.2-win-aarch64/geckodriver'  # Later in environment variable!
     WEB_DRIVER_PATH = 'Drivers/chromedriver/chromedriver'  # Later in environment variable!
     # options
     chrome_options = webdriver.ChromeOptions()
 
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    # When the sandbox is disabled using the flag option --no-sandbox, websites or rendered pages
+    # can potentially execute malicious Javascript based exploits on your computer.
+    # chrome_options.add_argument('--no-sandbox')
+
+    # browser window wouldn’t be visible
+    # chrome_options.add_argument('--headless')
+
+    # Only added when CI system environment variable is set or when inside a docker instance.
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+
+    # server requisites
+    # chrome_options.add_argument("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    # my PC requisites
+    chrome_options.add_argument(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36')
+
+    # after adding this block "https://intoli.com/" makes available (WebDriver - present (failed))
+    # check form: https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+
+    # after adding this block "https://intoli.com/" makes available (WebDriver - missing (passed))
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+    if proxy:
+        chrome_options.add_argument('--proxy-server=%s' % proxy)
 
     # options.headless = True
     driver = webdriver.Chrome(executable_path=WEB_DRIVER_PATH,
-                                options=chrome_options)
+                              options=chrome_options)
     return driver
 
 
@@ -42,12 +65,7 @@ def log_time(logger, description=''):
             exec_time = round(end - start, 2)
             logger.info(f'Parsing time {description} ({func.__name__}): {exec_time}s')
             return result
+
         return wrapper
+
     return inner
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,42 +1,64 @@
 import os
+import time
 
 import requests
 from requests.auth import HTTPBasicAuth
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from db_functions import get_table
+from functions_secondary import create_driver
+
+from dotenv import load_dotenv, find_dotenv
+import os
+
+# Конфиденциальные данные
+load_dotenv(find_dotenv())  # погрузка .env
 
 
-def get_alive_proxy_list():
-    directory = 'Proxies'
-
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            print(f)
-
-
-def get_facebook_response(url, auth, timeout):
+def auth_facebook_via_selenium(website):
     proxies_df = get_table('facebook.db', 'proxies')
     proxies = proxies_df['proxy'].values
-    resp = 'None'
 
     for proxy in proxies:
+
         try:
-            resp = requests.get(url,
-                                proxies={
-                                    "http": "http://" + proxy,
-                                    "https": "http://" + proxy,
-                                },
-                                auth=auth,
-                                timeout=timeout)
-            if resp.status_code == 200:
-                print(proxy)
-                print(resp)
-                break
+            driver = create_driver(proxy)
+            driver.get(website)
         except:
             continue
 
-    return resp
+        empty_page_html = '<html><head></head><body></body></html>'
+        empty_str = ''
+        print(driver.page_source)
+        if driver.page_source in (empty_page_html, empty_str):
+            continue
+        else:
+            break
+
+    # authorization
+    # fill email
+    email_field = driver.find_element(By.NAME, "email")
+    email_field.clear()
+    email_field.send_keys(FACEBOOK_EMAIL)
+    time.sleep(1)
+
+    # fill email
+    pass_field = driver.find_element(By.NAME, "pass")
+    pass_field.clear()
+    pass_field.send_keys(FACEBOOK_PASS)
+    time.sleep(1)
+
+    # pass_field.send_keys(Keys.ENTER)
+
+    loginbutton = driver.find_element(By.NAME, "login")
+    loginbutton.click()
+    time.sleep(1)
+
+    driver.close()
+    driver.quit()
 
 
 if __name__ == '__main__':
@@ -45,8 +67,6 @@ if __name__ == '__main__':
 
     url_root = 'https://www.facebook.com'
     url = 'https://www.facebook.com/groups/itpeopleconnection/members'
+    LOGIN_URL = "https://m.facebook.com/login.php?refsrc=https%3A%2F%2Fm.facebook.com%2F&amp;refid=8"
 
-    auth = HTTPBasicAuth(FACEBOOK_EMAIL, FACEBOOK_PASS)
-    get_facebook_response(url, auth, timeout=10)
-
-    pass
+    auth_facebook_via_selenium(LOGIN_URL)
